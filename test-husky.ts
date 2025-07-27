3d22f0c85789e5c85a1fd1c6a3d7f4405cef9a8f
Phan Đỉnh Thanh Hoàn <hoan.pdt@supertek.vn>
2025-07-27 10:31:19 +0700

    Configure husky and lint-staged for pre-commit hooks
    - Add husky and lint-staged as dev dependencies
    - Configure pre-commit hook to run lint-staged
    - Set up lint-staged to run ESLint and Prettier on staged files
    - Add eslint and eslint-config-next dependencies for proper linting
    - Update package.json scripts with prepare script for husky

--- test-husky.ts
+++
@@ -1,6 +0,0 @@
-// Test file to verify husky and lint-staged setup
-const testFunction = () => {
-  console.log('This should be formatted by prettier and linted by eslint');
-};
-
-export default testFunction;
