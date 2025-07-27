9dc4824222589d018c37a5a417e21d72ec09a346
Phan Đỉnh Thanh Hoàn <hoan.pdt@supertek.vn>
2025-07-27 10:30:33 +0700

    Test husky and lint-staged setup

---
+++ test-husky.ts
@@ -0,0 +1,6 @@
+// Test file to verify husky and lint-staged setup
+const testFunction = () => {
+  console.log('This should be formatted by prettier and linted by eslint');
+};
+
+export default testFunction;
